bbf2d4f796815e8dc2cd32b16ff7bb604a727c67
leonardo <pachecodossantos17@gmail.com>
2024-03-17 11:53:05 -0300

    cmp

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -6,9 +6,6 @@ return { -- Autocompletion
     {
       "L3MON4D3/LuaSnip",
       build = (function()
-        -- Build Step is needed for regex support in snippets
-        -- This step is not supported in many windows environments
-        -- Remove the below condition to re-enable on windows
         if vim.fn.has "win32" == 1 or vim.fn.executable "make" == 0 then
           return
         end
@@ -99,8 +96,8 @@ return { -- Autocompletion
       sources = {
         { name = "nvim_lsp" },
         { name = "luasnip" },
-        { name = "buffer" },
         { name = "nvim_lua" },
+        { name = "buffer" },
         { name = "path" },
       },
     }
